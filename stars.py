f585a12222ce6e0e6443c539762f1f648c2a2db5
Daniel Pope <mauve@mauveweb.co.uk>
2018-08-10 09:01:45 +0100

    Improve stars effect

--- stars.py
+++ stars.py
@@ -1,21 +1,23 @@
 import time
 from itertools import count
 import random
+from collections import deque
 
-from lights import Grid, WHITE, YELLOW
+from lights import Grid
 
 
 
 grid = Grid()
 
-rising = [None] * 60
+rising = deque(maxlen=30)
 
 
-for _ in grid.fps(30):
+for t in grid.fps(60):
     for (x, y), v in grid.items():
         grid[x, y] = tuple(max(c - 5, 0) for c in v)
 
-    rising[random.randrange(len(rising))] = grid.rand_pos()
+    if t % 5 == 0:
+        rising.append(grid.rand_pos())
 
     for pos in rising:
         if pos is None:
