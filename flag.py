d59791729fffd342eadf8a37ac3d3da77237faa5
Daniel Pope <mauve@mauveweb.co.uk>
2018-08-11 08:22:00 +0100

    Slow down flag

--- flag.py
+++ flag.py
@@ -7,6 +7,6 @@ from itertools import count
 COLORS = [RED, WHITE, GREEN, BLACK]
 
 grid = Grid()
-for t in grid.fps(20):
+for t in grid.fps(2):
     for x, y in grid.keys():
         grid[x, y] = COLORS[(x + t) // 4 % len(COLORS)]
