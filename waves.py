0f8fa10df96fbaaad8f83acb9d99662c860fa5ac
Daniel Pope <mauve@mauveweb.co.uk>
2018-08-15 00:05:44 +0100

    Add volume-based intensity

--- waves.py
+++ waves.py
@@ -17,16 +17,18 @@ with track_beats() as tracker:
     for f in grid.fps(60):
         grid.darken((0.6, 0.7, 0.9))
         newwaves = []
-        for x, y, vx in waves:
+        for x, y, vx, intensity in waves:
             rx = round(x)
-            grid[rx, y] = WHITE
+            c = round(min(intensity, 1.0) * 255)
+            grid[rx, y] = (c, c, c)
             if rx < x < grid.W - 1:
-                grid[rx + 1, y] = (255 * 2 * (x - rx),) * 3
+                grid[rx + 1, y] = (c * 2 * (x - rx),) * 3
             x += vx
             if x + 0.5 < grid.W:
-                newwaves.append((x, y, vx))
+                newwaves.append((x, y, vx, intensity * 0.98))
         waves = newwaves
 
         if tracker.has_beats():
+            intensity = min(tracker.vol, 0.1) / 0.05
             for y in range(grid.H):
-                waves.append((0, y, random.uniform(0.5, 0.55)))
+                waves.append((0, y, random.uniform(0.5, 0.55), intensity))
