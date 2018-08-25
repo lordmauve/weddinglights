b3f192fc08e968c8e99840098d03da9d7a26e77e
Daniel Pope <mauve@mauveweb.co.uk>
2018-08-25 09:10:07 +0100

    Tune duration of stars

--- stars.py
+++ stars.py
@@ -13,13 +13,12 @@ rising = deque(maxlen=30)
 
 
 for t in grid.fps(60):
-    for (x, y), v in grid.items():
-        grid[x, y] = tuple(max(c - 5, 0) for c in v)
+    grid.darken(0.95)
 
-    if t % 5 == 0:
+    if t % 3 == 0:
         rising.append(grid.rand_pos())
 
     for pos in rising:
         if pos is None:
             continue
-        grid[pos] = tuple(min(255, c + 10) for c in grid[pos])
+        grid[pos] = tuple(min(255, c + 20) for c in grid[pos])
