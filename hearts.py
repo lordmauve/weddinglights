331d5a8e9fcfdc7a21ddbf2884034e6add7dad76
Daniel Pope <mauve@mauveweb.co.uk>
2018-08-25 09:09:41 +0100

    Resize heart

--- hearts.py
+++ hearts.py
@@ -1,20 +1,16 @@
 from lights import Grid
 
-from pygame import Surface
 from pygame.image import load
-from pygame import surfarray
 
 
 heart = load('images/heart.png')
 
-screen = Surface((64, 8), 0, 24)
-
-
 grid = Grid()
+screen = grid.surface()
+
+
 for t in grid.fps(3):
     screen.fill((0, 0, 0))
-    for x in range(0, 64, 13):
+    for x in range(0, 64, 32):
         screen.blit(heart, ((t + x) % 64, 0))
-    buf = surfarray.array3d(screen)
-    buf = buf.transpose([1, 0, 2])[:, ::-1, :]
-    grid.pixels = buf.reshape(64 * 8, 3)
+    grid.blitsurf(screen)
